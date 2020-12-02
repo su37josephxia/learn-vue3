2d3df9da9a5f60cbfc9d824879e6bcb3db3e311d
josephxia <15906475@qq.com>
2020-12-02 16:37:13 +0800

    refact suspense

--- vue-mastery/composition-api/src/components/Suspense/Event.vue
+++ vue-mastery/composition-api/src/components/Suspense/Event.vue
@@ -8,12 +8,24 @@ export default {
   async setup() {
     const count = ref(null);
     function getEventCount() {
-      return new Promise((resolve) => {
-        setTimeout(() => resolve(3), 1000);
+      return new Promise((resolve, reject) => {
+        if (Math.random() > 0.5) {
+          console.log('error.....',Math.random())
+          // 随机返回时间
+          reject('get Event Error.');
+        } else {
+          // 随机出错
+          setTimeout(() => resolve(3), 1000);
+        }
       });
     }
 
-    count.value = await getEventCount();
+    try {
+      count.value = await getEventCount();
+    } catch (e) {
+      throw e
+    }
+
     return { count };
   },
 };
--- vue-mastery/composition-api/src/components/Suspense/Suspense.vue
+++ vue-mastery/composition-api/src/components/Suspense/Suspense.vue
@@ -1,17 +1,38 @@
 <template>
-  <Suspense>
-    <template #default>
-      <Event />
-    </template>
-    <template #fallback> Loading.... </template>
-  </Suspense>
+  <div>
+    <div v-if="error">Uh oh .. {{ error }}</div>
+    <Suspense>
+      <template #default>
+        <div>
+          <Event />
+          <AsyncEvent />
+        </div>
+      </template>
+      <template #fallback> Loading.... </template>
+    </Suspense>
+  </div>
 </template>
 
 <script>
+import { ref, onErrorCaptured, defineAsyncComponent } from "vue";
+
 import Event from "./Event.vue";
+
+const AsyncEvent = defineAsyncComponent(() => import("./Event.vue"));
 export default {
   components: {
     Event,
+    AsyncEvent,
+  },
+
+  setup() {
+    const error = ref(null);
+    onErrorCaptured((e) => {
+      error.value = e;
+      // 阻止错误继续冒泡
+      return true;
+    });
+    return { error };
   },
 };
 </script>
